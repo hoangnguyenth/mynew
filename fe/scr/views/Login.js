7a93ffa8b5d704290e2592e462d0d501315f4ce1
nthoang <104285015+hoangnguyenth@users.noreply.github.com>
2022-11-23 19:10:35 +0700

    2th

--- fe/scr/views/Login.js
+++ fe/scr/views/Login.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import axios from 'axios'
+import {Button, Input, Box} from '@mui/material'
+import { useForm } from 'react-hook-form'
+import {useValue} from '../Context'
+import { useNavigate } from "react-router-dom"
 
-const Login = () => <h1>Log in</h1>
+const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+    const [dt, setDt] = useValue()
+    const navigate = useNavigate();
+      return (
+        <form onSubmit={handleSubmit(async (data) => {
+            const r = await axios.post("https://nodejs-fake-api.herokuapp.com/login",data)
+            await setDt(r.data)
+            navigate("/")
+            })}>
+            <Box sx={{display:"flex", flexDirection:"column"}}>
+                <Input type='username' placeholder='Username' {...register('username', { required: true })} />
+                {errors.username && <p>Username is required.</p>}
+
+                <Input type='password' placeholder='Password' {...register('pass', { required: true })} />
+                {errors.pass && <p>Password is required.</p>}
+
+                <Button type="submit" variant='contained'>Login</Button>
+            </Box>
+        </form>
+      );
+}
 
 export default Login
