d6f55b54b4619e4cd87e9e9f16daec94cbe341be
nthoang <thoang3377@gmail.com>
2022-11-23 11:16:56 +0000

    1th

--- fe/scr/App.js
+++ fe/scr/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './views/Home'
 import Login from './views/Login'
-
-
 const App = () => 
 <Routes>
     <Route path='/' element = {<Home />}/>
--- fe/scr/index.js
+++ fe/scr/index.js
@@ -1,6 +1,7 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
-import {createRoot} from 'react-dom/client'
 
-const root = createRoot(document.getElementById("root"))
-root.render( <BrowserRouter> <App /> </BrowserRouter> )
+createRoot(document.getElementById("root"))
+.render( <BrowserRouter> <App /> </BrowserRouter>  )
